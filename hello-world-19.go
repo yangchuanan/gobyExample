2f40a3d6032c22ed5c9d71fdfc22d4c2315d35eb
yangchuanan <yang_an1991@163.com>
2020-04-08 19:40:49 +0800

    file learning

---
+++ hello-world-19.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readFileDemo() {
+	dat, err := ioutil.ReadFile("D:\\GoProject\\gobyExample\\fileTest.txt")
+	check(err)
+	fmt.Println(string(dat))
+
+	f, err := os.Open("D:\\GoProject\\gobyExample\\fileTest.txt")
+	check(err)
+
+	b1 := make([]byte, 5)
+	n1, err := f.Read(b1)
+	check(err)
+	fmt.Printf("%d bytes:%s\n", n1, string(b1[:n1]))
+
+	o2, err := f.Seek(6, 0)
+	check(err)
+	b2 := make([]byte, 2)
+	n2, err := f.Read(b2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: ", n2, o2)
+	fmt.Printf("%v\n", string(b2[:n2]))
+
+	o3, err := f.Seek(6, 0)
+	check(err)
+	b3 := make([]byte, 2)
+	n3, err := io.ReadAtLeast(f, b3, 2)
+	check(err)
+	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
+
+	// 返回文件起始位置
+	_, err = f.Seek(0, 0)
+	check(err)
+
+	r4 := bufio.NewReader(f)
+	b4, err := r4.Peek(5)
+	check(err)
+	fmt.Printf("5 bytes: %s\n", string(b4))
+
+	f.Close()
+
+}
+
+func main() {
+	d1 := []byte("hello\ngo\n")
+	err := ioutil.WriteFile("D:\\GoProject\\gobyExample\\writeFile.txt", d1, 0644)
+	check(err)
+
+	f, err := os.Create("D:\\GoProject\\gobyExample\\writeTest1.txt")
+	check(err)
+
+	defer f.Close()
+
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	fmt.Printf("wrote %d bytes\n", n2)
+
+	n3, err := f.WriteString("writes\n")
+	fmt.Printf("wrote %d bytes\n", n3)
+	// 将缓冲的数据写入硬盘
+	f.Sync()
+
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	fmt.Printf("wrote %d bytes\n", n4)
+	//使用 Flush 来确保，已将所有的缓冲操作应用于底层 writer。
+	w.Flush()
+
+}
